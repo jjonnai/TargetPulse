7edb6433c8616b662f0b4406c653c22f4a5d5f4c
Jonna Huotari <t1hujo04@students.oamk.fi>
2023-08-30 15:48:02 +0300

    ready

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
-  const [age, setAge] = useState('');
-  const [low, setLow] = useState('');
-  const [high, setHigh] = useState('');
+  const [age, setAge] = useState(' ');
+  const [low, setLow] = useState(0);
+  const [high, setHigh] = useState(0);
   
 
-  function calculate(text){
-    setAge(text);
-    if (age){
-      const resultlow = (220-age)*0.65;
-     setLow(resultlow)
-      const resulthigh = (220-age)*0.85;
-      setHigh(resulthigh);
-    }
+  function calculate(age){
+    setAge(age);
+    
+      const low = (220-age) * 0.65;
+      setLow(low)
+      const high = (220-age) * 0.85;
+      setHigh(high)
   }
   
   return (
     <View style={styles.container}>
       <Text style={styles.field}>Age</Text>
-      <TextInput style= {styles.input} value= {age} onChangeText={text => calculate(text)} ></TextInput>
+      <TextInput style= {styles.input} value= {age} onChangeText={age => calculate(age)} keyboardType='decimal-pad' ></TextInput>
       <Text style={styles.field} onChangeText={text => calculate(text)}>Hr limits</Text>
-      <Text style={styles.field}>{low.toFixed(0)}-{high.toFixed(0)} </Text>
+      <Text style={styles.field}>{low.toFixed()}-{high.toFixed()} </Text>
     </View>
   );
 }
